f633eafa22786db1054665901e219c27d6ff8c0d
Jason Chen <jinghao_chen@msn.com>
2022-05-20 12:48:51 +0800

    增加3题

---
+++ 牛客习题训练/简单/HJ34 图片整理.py
@@ -0,0 +1,48 @@
+# 描述
+# Lily上课时使用字母数字图片教小朋友们学习英语单词，每次都需要把这些图片按照大小（ASCII码值从小到大）排列收好。请大家给Lily帮忙，通过代码解决。
+# Lily使用的图片使用字符"A"到"Z"、"a"到"z"、"0"到"9"表示。
+
+# 数据范围：每组输入的字符串长度满足 1 ≤ n ≤ 1000 
+
+# 输入描述：
+# 一行，一个字符串，字符串中的每个字符表示一张Lily使用的图片。
+
+# 输出描述：
+# Lily的所有图片按照从小到大的顺序输出
+# while True:
+#     try:
+#         print(''.join(sorted(input())))
+#     except EOFError: break
+
+while True:
+    try:
+        a = input()
+        a = list(a) #将字符串放入列表中，每个字符为一项
+        for i in range(len(a)):
+            a[i] = ord(a[i]) #先转化为ASCLL码
+        a.sort() #按照ASCLL排序
+        for i in range(len(a)): 
+            a[i] = chr(a[i]) #再将每个ASCLL还原为字符
+        print(''.join(a)) #按要求输出
+    except:
+        break
+
+# s = input()
+# s_lsit=[] # 这个空列表用于装字符串全部转换后的ASCII码
+# for item in s:
+#     s_lsit.append(ord(item)) #遍历每一个元素，转换为ASCII码，追加进列表
+#     pass
+# news_list=sorted(s_lsit) # ACSII码列表排序
+# res_list=[]# 这个空列表装排序后各个ASCII码转回字符
+# for item in news_list:
+#     res_list.append(chr(item))# 遍历每一个ASCII码，转换为字符，追加列表
+#     pass
+# print("".join(res_list)) # 将列表打印为字符串
+# pass
+# while True:
+#     try:
+#         s=str(input())
+#         func(s)
+#         pass
+#     except:
+#         break
---
+++ 牛客习题训练/简单/HJ35 蛇形矩阵.py
@@ -0,0 +1,64 @@
+# 描述
+# 蛇形矩阵是由1开始的自然数依次排列成的一个矩阵上三角形。
+
+# 例如，当输入5时，应该输出的三角形为：
+# 1 3 6 10 15
+# 2 5 9 14
+# 4 8 13
+# 7 12
+# 11
+
+# 输入描述：
+# 输入正整数N（N不大于100）
+
+# 输出描述：
+# 输出一个N行的蛇形矩阵。
+while True:
+    try:
+        num = int(input())
+        for i in range(num):
+            if i == 0:
+                res = [(x+2)*(x+1)//2 for x in range(num)]
+            else:
+                res = [x - 1 for x in res[1:]]
+            print(' '.join(map(str, res)))
+    except:
+        break
+
+# 另一种思路：
+# 把蛇形矩阵旋转45度其实就是个金字塔，最上面是1，最右下方是最大的数，即10
+# 原本：
+# 1 3 6 10
+# 2 5 9
+# 4 8
+# 7
+# 旋转一下：
+# 1
+# 2 3
+# 4 5 6
+# 7 8 9 10
+# list1就是旋转后的矩阵
+# 然后可以发现旋转后的矩阵里每一行的最后一个元素，就是蛇形矩阵中第一行的元素，倒数第二个元素就是第二行的，依次类推。
+# 解法：
+# while 1:
+#     try:
+#         n = int(input())
+#         list1 = []
+#         for i in range(1,n+1):
+#             list1.append([0]*i)
+#         a = 0
+#         for i in range(n):
+#             for j in range(i+1):
+#                 a = a + 1
+#                 list1[i][j]=a
+#         list2,she = [],[]
+#         for i in range(1,n+1):
+#             for line in list1:
+#                 if line:
+#                     list2.append(line.pop(-1))
+#             she.append(" ".join(map(str,list2)))
+#             list2 = []
+#         for i in she:
+#             print(i)
+#     except:
+#         break
